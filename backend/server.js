c518b8c94f287c82cd072ef271b2abf3ec3d88b7
Deepak Kumar <mahtodeepak857@gmail.com>
2024-04-04 16:33:29 +0530

    Creating simple express server and adding dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-const PORT = 5000;
+const port = 5000;
 
 const app = express();
 
 app.get("/", (req, res) => res.send("server is ready"));
 
+app.listen(port, () => console.log(`Server started on port ${port}`));
