95df4d7c3105de4c98d90917bcbe805e3993998c
Deepak Kumar <mahtodeepak857@gmail.com>
2024-04-07 02:46:33 +0530

    Login functionality and react-toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import React from "react";
 import Header from "./components/Header.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Container className="my-2">
         <Outlet />
       </Container>
